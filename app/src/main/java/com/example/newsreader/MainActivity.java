897a6d1c4075f0c7daf3925d96766b7670e54aff
Marko Šimić <sasa.simic.98@gmail.com>
2019-05-21 17:47:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/newsreader/MainActivity.java
@@ -0,0 +1,177 @@
+package com.example.newsreader;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.newsreader.Models.Article;
+import com.example.newsreader.Models.News;
+import com.example.newsreader.NewsAPI.ApiClient;
+import com.example.newsreader.NewsAPI.ApiInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
+
+    public static final String API_KEY = "4e7f3f5a52944ba6abb33d4abc19740a";
+
+    @BindView(R.id.recyclerView) RecyclerView recyclerView;
+    @BindView(R.id.swipe_refresh_layout) SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.topheadelines) TextView topHeadLine;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private NewsAdapter adapter;
+    private String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        onLoadingSwipeRefresh("");
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+
+    }
+
+    //region LoadJson
+    private void loadJson(final String keyWord) {
+
+        topHeadLine.setVisibility(View.INVISIBLE);
+        swipeRefreshLayout.setRefreshing(true);
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        String country = Utils.getCountry();
+        String language = Utils.getLanguage();
+        Call<News> call;
+        if (keyWord.length() > 0){
+            call = apiInterface.getNewsSearch(keyWord , language , "publishedAt" , API_KEY);
+        } else {
+
+            call = apiInterface.getNews(country,API_KEY);
+
+        }
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful() && response.body().getArticle() != null){
+
+                    if (!articles.isEmpty()){
+                        articles.clear();
+                    }
+                    articles = response.body().getArticle();
+                    adapter = new NewsAdapter(articles,MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+                    topHeadLine.setVisibility(View.VISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
+
+                }else {
+
+                    topHeadLine.setVisibility(View.INVISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
+                    Toast.makeText(MainActivity.this, "No result", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+                topHeadLine.setVisibility(View.VISIBLE);
+                //swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+    }
+    //endregion
+
+    //regionMenuItems
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setQueryHint("Search Latest News ...");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                if (query.length() > 2){
+                    onLoadingSwipeRefresh(query);
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchMenuItem.getIcon().setVisible(false,false);
+
+
+        return true;
+    }
+
+
+    //endregion
+
+    //region Swipe Refresh Layout
+    @Override
+    public void onRefresh() {
+
+        loadJson("");
+
+    }
+
+    private void onLoadingSwipeRefresh(final String keyword){
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                loadJson(keyword);
+            }
+        });
+    }
+    //endregion
+
+}
